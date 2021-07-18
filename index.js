535146311da6ad9a1777370ce7eb9de9a86e4fa3
Isabelle-github <ixavierayix@yahoo.fr>
2021-07-18 17:51:20 +0200

    team draft

--- index.js
+++ index.js
@@ -4,11 +4,41 @@ const PORT = process.env.PORT || 3000
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 const fileSyst = require('fs')
+const appName = 'RESTAURANT'
 
 console.log('satrt')
 app.get('/', (req, res) => {
     // res.send('index')
-    res.render('index')
+    res.render('index', { myPageTitle: `${appName}|HOME` })
 })
+app.get('/contact', (req, res) => {
+    // res.send('index')
+    res.render('contact', { myPageTitle: `${appName}|CONTACT` })
+})
+app.get('/events', (req, res) => {
+    // res.send('index')
+    res.render('events', { myPageTitle: `${appName}|EVENTS` })
+})
+app.get('/feedback', (req, res) => {
+    // res.send('index')
+    res.render('feedback', { myPageTitle: `${appName}|FEEDBACK` })
+})
+app.get('/menu', (req, res) => {
+    // res.send('index')
+    res.render('menu', { myPageTitle: `${appName}|MENU` })
+})
+app.get('/team', (req, res) => {
+    // res.send('index')
+    res.render('team', { myPageTitle: `${appName}|TEAM` })
+})
+app.get('/galerie', (req, res) => {
+    // res.send('index')
+    res.render('galerie', { myPageTitle: `${appName}|GALERIE` })
+})
+app.get('/booking', (req, res) => {
+    // res.send('index')
+    res.render('booking', { myPageTitle: `${appName}|BOOKING` })
+})
+
 
 app.listen(PORT, () => { console.log(`listening to http://localhost:${PORT}`) })
